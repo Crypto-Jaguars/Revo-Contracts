e427d8b753ec2a6f91df8fbf2b91e06d31e6d341
Fabricio Robles <fabrobles92@gmail.com>
2025-02-21 16:08:44 -0600

    clean(lib.rs): clean unused code

--- ContractsRevo/product-auction-contract/src/lib.rs
+++ ContractsRevo/product-auction-contract/src/lib.rs
@@ -1,10 +1,6 @@
 #![no_std]
 use datatype::{Auction, AuctionError, DataKeys};
-use soroban_sdk::{contract, contractimpl, Address, Env, String, Symbol, Vec};
-
-// use crate::datatype::{
-//     DataKeys, ProductRatings, PurchaseReviewError, PurchaseVerificationData, ReviewDetails,
-// };
+use soroban_sdk::{contract, contractimpl, Address, Env};
 
 mod product_auction;
 mod listing;
@@ -21,7 +17,7 @@ pub struct ProductAuctionContract;
 
 #[contractimpl]
 impl ProductAuctionContract {
-    
+
     pub fn get_auction(env: Env, seller: Address, product_id: u128) -> Result<Auction, AuctionError> {
         let key = &DataKeys::Auction(seller.clone(), product_id);
         env.storage().instance().get(key).ok_or(AuctionError::AuctionNotFound)
