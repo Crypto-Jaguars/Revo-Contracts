6f94750d29636dd9a240f5703b4cf02a0995120c
Ryzen XP <iron4540@gmail.com>
2025-04-27 15:29:12 +0530

    init

---
+++ ContractsRevo/environmental-impact-tracking/src/lib.rs
@@ -0,0 +1,13 @@
+#![no_std]
+use soroban_sdk::{contract, contractimpl, vec, Env, String, Vec};
+
+#[contract]
+pub struct EnvironmentalContract;
+
+#[contractimpl]
+impl EnvironmentalContract {
+  
+}
+
+mod test;
+
